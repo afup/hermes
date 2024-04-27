49940b3ab4a60d76cb15ce32951614143143807e
Baptiste Leduc <baptiste.leduc@gmail.com>
2024-04-27 17:35:10 +0200

    Send a message to the Driver when someone joins their Transport

--- app/src/Discord/Command/JoinTransportCommand.php
+++ app/src/Discord/Command/JoinTransportCommand.php
@@ -8,6 +8,7 @@ use Afup\Hermes\Discord\Command\Helper\EventHelper;
 use Afup\Hermes\Discord\Command\Helper\OptionHelper;
 use Afup\Hermes\Discord\Command\Helper\UserHelper;
 use Afup\Hermes\Entity\Traveler;
+use Afup\Hermes\Enum\Direction;
 use Afup\Hermes\Enum\Traveler as TravelerType;
 use Afup\Hermes\Repository\Event\FindEventByChannel;
 use Afup\Hermes\Repository\Transport\GetTransportForEvent;
@@ -17,6 +18,7 @@ use Discord\Builders\MessageBuilder;
 use Discord\Discord;
 use Discord\Parts\Interactions\Command\Option as CommandOption;
 use Discord\Parts\Interactions\Interaction;
+use Discord\Parts\User\User;
 use Doctrine\ORM\EntityManagerInterface;
 
 final readonly class JoinTransportCommand implements CommandInterface
@@ -51,8 +53,8 @@ final readonly class JoinTransportCommand implements CommandInterface
 
     public function callback(Discord $discord): void
     {
-        $discord->listenCommand(self::COMMAND_NAME, function (Interaction $interaction) {
-            if ($interaction->user?->bot ?? false) {
+        $discord->listenCommand(self::COMMAND_NAME, function (Interaction $interaction) use ($discord) {
+            if (null === $interaction->user || $interaction->user->bot) {
                 return; // ignore bots
             }
 
@@ -76,7 +78,11 @@ final readonly class JoinTransportCommand implements CommandInterface
 
             $transportDriver = $transport->getDriver();
             $interaction->respondWithMessage(MessageBuilder::new()->setContent(sprintf(':bust_in_silhouette: You are now riding in Transport `%s`.', $transport->shortId)), true);
-            $interaction->user?->sendMessage(MessageBuilder::new()->setContent(sprintf('Thanks for sharing a ride with <@%d>, if you want more details about the transport please send DM to the transport creator: <@%d>', $transportDriver->userId, $transportDriver->userId)));
+            $interaction->user->sendMessage(MessageBuilder::new()->setContent(sprintf('Thanks for sharing a ride with <@%d>, if you want more details about the transport please send DM to the transport creator: <@%d>', $transportDriver->userId, $transportDriver->userId)));
+            $discord->users->fetch((string) $transportDriver->userId)->then(function (User $user) use ($transport, $interaction) {
+                $direction = sprintf(Direction::EVENT === $transport->direction ? 'from %s to the event' : 'from the event to %s', $transport->postalCode);
+                $user->sendMessage(MessageBuilder::new()->setContent(sprintf('A new co-traveler joined your transport %s (%s), you can send him a message: <@%s>', $direction, $transport->startAt->format(\DateTimeInterface::ATOM), $interaction->user->id)));
+            });
         });
     }
 }
