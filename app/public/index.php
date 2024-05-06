ae1abe8a06c29500092326fd694a8107e5eb1c50
Baptiste Leduc <baptiste.leduc@gmail.com>
2024-05-06 18:07:17 +0200

    Make a simple OK output on public

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,3 @@
 <?php
 
-use Afup\Hermes\Kernel;
-
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+echo 'OK';
