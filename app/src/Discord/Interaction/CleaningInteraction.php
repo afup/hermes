36418a66e137d48e3f0d83381b22c4d7bd3d1d25
Baptiste Leduc <baptiste.leduc@gmail.com>
2024-05-14 11:23:41 +0200

    cs-fix

--- app/src/Discord/Interaction/CleaningInteraction.php
+++ app/src/Discord/Interaction/CleaningInteraction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Afup\Hermes\Discord\Interaction;
 
-use Discord\Builders\MessageBuilder;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
